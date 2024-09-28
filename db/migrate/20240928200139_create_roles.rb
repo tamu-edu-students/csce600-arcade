87bb131edadac71fd3a4e0935789690a788094e7
Antonio Rosales <a.ros.vie@hotmail.com>
2024-09-28 15:06:58 -0500

    basic model migration

--- db/migrate/20240928200139_create_roles.rb
+++ db/migrate/20240928200139_create_roles.rb
@@ -2,7 +2,11 @@ class CreateRoles < ActiveRecord::Migration[7.2]
   def change
     create_table :roles do |t|
       t.string :name
+<<<<<<< HEAD
       t.references :user, foreign_key: true
+=======
+      t.references :user, null: false, foreign_key: true
+>>>>>>> ef609d2 (basic model migration)
 
       t.timestamps
     end
