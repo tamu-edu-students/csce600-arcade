c1cf190db8ba477b63b6857fac54fabd8267ff98
Antonio Rosales <a.ros.vie@hotmail.com>
2024-09-28 15:06:58 -0500

    basic model migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_041050) do
     t.string "name"
     t.string "game_path"
   end
-  
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "session"
-    t.string "roleId"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -35,4 +26,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_041050) do
     t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_roles_on_user_id"
+  end
+
+  add_foreign_key "roles", "users"
 end
