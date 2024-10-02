8483c219cd22615b302053adaf4d563b7989b60d
Antonio Rosales <a.ros.vie@hotmail.com>
2024-10-02 11:22:02 -0500

    tweaked schema for user

--- db/migrate/20241002035856_create_users.rb
+++ db/migrate/20241002035856_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.2]
       t.string :name
       t.string :email
       t.string :session
-      t.string :roleId
+      t.references :role, null: false, foreign_key: true
 
       t.timestamps
     end
