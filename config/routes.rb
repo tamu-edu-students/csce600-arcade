2c19bd3c023eb03b6885e5f2eb11a99381c60cf2
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2024-10-02 15:20:34 -0500

    add back deleted health check route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :games
   root :to => redirect('/games')
 
+  get "up" => "rails/health#show", as: :rails_health_check
+
   ## stub paths to demo game landing page
   get '/spellingbee/:id', to: 'games#demo_game', as: 'spellingbee'
   get '/wordle/:id', to: 'games#demo_game', as: 'wordle'
