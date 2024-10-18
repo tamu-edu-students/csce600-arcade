82ad86da41d66c3bc8d42c44415ccc61880f2d55
Krishna Calindi <kxc@tamu.edu>
2024-10-17 22:58:36 -0500

    rack cors 2.0

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,14 @@
 # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://csce606arcade-12ac8dd4dc24.herokuapp.com' 
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :options],
-        credentials: true
-    end
+      origins "http://localhost:4000"
+              "http://127.0.0.1:4000",
+              "https://csce606arcade-12ac8dd4dc24.herokuapp.com",
   
-    allow do
-      origins 'http://127.0.0.1:3000' 
-      resource '*',
+      resource "*",
         headers: :any,
-        methods: [:get, :post, :options],
-        credentials: true
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true,
+        max_age: 86400
     end
   end
