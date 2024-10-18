86a63d0104dae196b19b50bab1fc18b416ca50d0
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2024-10-18 11:34:25 -0500

    merge in main, fix manifest.json and fix button on wordle game

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../javascript
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
