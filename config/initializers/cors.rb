47d28142cdc21ec76ebd86e2bf4f12f1e7a5acc0
Krishna Calindi <kxc@tamu.edu>
2024-10-17 23:05:07 -0500

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 # config/initializers/cors.rb
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://localhost:4000"
+      origins "http://localhost:4000",
               "http://127.0.0.1:4000",
-              "https://csce606arcade-12ac8dd4dc24.herokuapp.com",
+              "https://csce606arcade-12ac8dd4dc24.herokuapp.com"
   
       resource "*",
         headers: :any,
