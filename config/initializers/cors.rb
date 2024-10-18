e43d3bac1f99f5d8439f93470b398d495fa05cee
Antonio Rosales <a.ros.vie@hotmail.com>
2024-10-17 23:34:50 -0500

    test fix for auth

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,14 @@
-# config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # config/initializers/cors.rb
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://csce606arcade-12ac8dd4dc24.herokuapp.com' 
-      resource '*',
+      origins "http://localhost:3000",
+              "http://127.0.0.1:3000",
+              "https://rack-cors-roda.herokuapp.com"
+
+      resource "*",
         headers: :any,
-        methods: [:get, :post, :options],
-        credentials: true
-    end
-  
-    allow do
-      origins 'http://127.0.0.1:3000' 
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :options],
-        credentials: true
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+        credentials: true,
+        max_age: 86400
     end
   end
