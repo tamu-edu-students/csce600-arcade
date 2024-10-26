797266a38401eb2c1ff46f3b9620b827407a66bf
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2024-10-25 22:59:40 -0500

    fix settings being overwritten when other settings are changed

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,20 +4,14 @@ class SettingsController < ApplicationController
     settings = Settings.find_by(user_id: session[:user_id])
     if params[:settings].present? and !params[:settings][:game_font_casing].nil?
       settings.game_font_casing = params[:settings][:game_font_casing]
-    else
-      settings.game_font_casing = true
     end
 
     if params[:settings].present? and !params[:settings][:page_contrast].nil?
       settings.page_contrast = params[:settings][:page_contrast]
-    else
-      settings.page_contrast = 100
     end
 
     if params[:settings].present? and params[:settings][:active_roles].present?
       settings.active_roles = params[:settings][:active_roles].join(",")
-    else
-      settings.active_roles = ""
     end
 
     settings.save
