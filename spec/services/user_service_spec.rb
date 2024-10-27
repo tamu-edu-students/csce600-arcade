dca0dc50697f213ca09ed90c370001816bbb66c4
Krishna Calindi <kxc@tamu.edu>
2024-10-26 19:12:55 -0500

    updated documentation

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -199,4 +199,14 @@ RSpec.describe UserService do
       expect(fetched_users).to match_array(users)
     end
   end
+  
+  describe 'connections' do
+    let(:user) { User.new(id: 1, email: "test@tamu.edu", github_username: "testgit", spotify_username: "testspot", first_name: "Test", last_name: "User") }
+    it 'connects' do
+      auths =  UserService.fetch_connected_auths(user)
+      expect(auths["email"]).to eq("test@tamu.edu")
+      expect(auths["github"]).to eq("testgit")
+      expect(auths["spotify"]).to eq("testspot")
+    end
+  end
 end
