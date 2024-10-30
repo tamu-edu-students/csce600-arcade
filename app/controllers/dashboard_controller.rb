35bc7c5b1877c4a1b213a8785bc06f214913b72c
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2024-10-30 18:44:58 -0500

    SCRUM-53: fix cognitive complexity on dashboard code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,29 +3,35 @@ class DashboardController < ApplicationController
 
     def show
       user_id = session[:user_id]
-      games = Game.where.not(id: -1)
+      
       @dashboard_details = {}
+
       total_games_played = Dashboard.where(user_id: user_id).where.not(game_id: -1).count
       @dashboard_details["total_games_played"] = total_games_played
-      last_played_record = Dashboard.where(user_id: user_id).order(played_on: :desc).first
-      last_played_on = last_played_record ? time_ago_in_words(last_played_record.played_on) : "Never played"
-      @dashboard_details["last_played_on"] = last_played_on
-      streak_record = Dashboard.where(user_id: user_id, game_id: -1, streak_record: true).first
-      @dashboard_details["streak"] = streak_record ? streak_record.streak_count : 0
-  
 
+      last_played_record = Dashboard.where(user_id: user_id).order(played_on: :desc).first
+      @dashboard_details["last_played_on"] = time_ago_in_words(last_played_record&.played_on)
+
+      streak_record = Dashboard.where(user_id: user_id, game_id: -1, streak_record: true).first
+      @dashboard_details["streak"] = streak_record&.streak_count || 0
+  
+      populate_game_stats(user_id)
+      
+    end
+
+    private
+    def populate_game_stats(user_id)
+      games = Game.where.not(id: -1)
       games.each do |game|
         last_played_record = Dashboard.where(user_id: user_id, game_id: game.id).order(played_on: :desc).first
-        last_played_on = last_played_record ? time_ago_in_words(last_played_record.played_on) : "Never played"
         @dashboard_details[game.id] = { 
           "name" => game.name,
-          "last_played_on" => last_played_on,
+          "last_played_on" => time_ago_in_words(last_played_record&.played_on),
           "score" => Dashboard.where(user_id: user_id, game_id: game.id).sum(:score)
          }
       end
     end
 
-    private
     def time_ago_in_words(from_time)
       return "Never played" if from_time.nil?  # Handle nil input
     
