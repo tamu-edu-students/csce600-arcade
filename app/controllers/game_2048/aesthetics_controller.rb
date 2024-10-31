b605bfc9bfb82d3cceda0db3e123125b5b1e4183
Junchao Wu <wjc1298916267@gmail.com>
2024-10-31 08:00:19 -0500

    make the board static

--- app/controllers/game_2048/aesthetics_controller.rb
+++ app/controllers/game_2048/aesthetics_controller.rb
@@ -3,7 +3,6 @@ module Game2048
     before_action :set_aesthetic
 
     def edit
-      initialize_game if session[:game_2048_board].nil?
     end
 
     def update
@@ -16,7 +15,6 @@ module Game2048
 
     def preview
       if @aesthetic.update(aesthetic_params)
-        initialize_game if session[:game_2048_board].nil?
         render partial: "game2048/preview_board", layout: false
       else
         render json: { error: @aesthetic.errors.full_messages }, status: :unprocessable_entity
@@ -32,26 +30,6 @@ module Game2048
 
     def aesthetic_params
       params.require(:aesthetic).permit(:font, colors: [])
-    end
-
-    def initialize_game
-      session[:game_2048_board] = Array.new(4) { Array.new(4, 0) }
-      session[:game_2048_score] = 0
-      2.times { add_new_tile }
-    end
-
-    def add_new_tile
-      empty_cells = []
-      session[:game_2048_board].each_with_index do |row, i|
-        row.each_with_index do |cell, j|
-          empty_cells << [ i, j ] if cell == 0
-        end
-      end
-
-      if empty_cells.any?
-        cell = empty_cells.sample
-        session[:game_2048_board][cell[0]][cell[1]] = [ 2, 4 ].sample
-      end
     end
   end
 end
