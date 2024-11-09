a09934326bbadf1090fe2e782a5c844a4a06fe11
Krishna Calindi <kxc@tamu.edu>
2024-11-09 13:04:02 -0600

    mo

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe SessionsController, type: :controller do
       expect(flash[:notice]).to eq("You are logged out.")
     end
   end
+
+  describe '#omniauth' do
+  before do
+    @oauth_service = double('OauthService')
+    allow(OauthService).to receive(:new).and_return(@oauth_service)
+  end
+
+  it 'redirects on failure to welcome' do
+    allow(@oauth_service).to receive(:connect_user).and_return({ success: false })
+    get :omniauth
+    expect(response).to redirect_to(welcome_path)
+  end
+
+  it 'logs in on success' do
+    user = double('User', id: 1)
+    result = { success: true, user: user }
+    allow(@oauth_service).to receive(:connect_user).and_return(result)
+    get :omniauth
+    expect(session[:user_id]).to eq(user.id)
+  end
+end
 end
