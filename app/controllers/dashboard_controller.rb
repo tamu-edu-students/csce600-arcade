61cb61de3a605fce99d627df5014e34fe964e7ef
Krishna Calindi <kxc@tamu.edu>
2024-11-15 08:45:55 -0600

    fixed broken tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -26,6 +26,10 @@ class DashboardController < ApplicationController
       populate_game_stats(user_id)
     end
 
+    # This method sets game history for displayed to the user.
+    #
+    # Sets the following attributes:
+    # game, game_history
     def game_history
       @game = Game.find(params[:game_id])
       @game_history = Dashboard.where(user_id: session[:user_id], game_id: @game.id).order(played_on: :desc)
