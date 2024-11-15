c4e3d0753b05740cd39f3da9511ebbcf10251cf6
Nandinii Yeleswarapu <nandiniiys@tamu.edu>
2024-11-15 00:12:19 -0600

    added more rspec tests and fixed some failing cucumber tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,36 +10,43 @@ class DashboardController < ApplicationController
     # Sets the following attributes for each game:
     # name, last_played_on, score
     def show
-      user_id = session[:user_id]
-
       @dashboard_details = {}
 
-      total_games_played = Dashboard.where(user_id: user_id).where.not(game_id: -1).count
-      @dashboard_details["total_games_played"] = total_games_played
-
-      last_played_record = Dashboard.where(user_id: user_id).where.not(game_id: -1).order(played_on: :desc).first
-      @dashboard_details["last_played_on"] = time_ago_in_words(last_played_record&.played_on)
-
-      streak_record = Dashboard.where(user_id: user_id, game_id: -1, streak_record: true).first
-      @dashboard_details["streak"] = streak_record&.streak_count || 0
-
-      populate_game_stats(user_id)
+      populate_total_games_played
+      populate_last_played_on
+      populate_streak
+      populate_game_stats
     end
 
+    # This method sets the game history displayed to the user.
     def game_history
       @game = Game.find(params[:game_id])
       @game_history = Dashboard.where(user_id: session[:user_id], game_id: @game.id).order(played_on: :desc)
     end
 
     private
-    def populate_game_stats(user_id)
+    def populate_streak
+      streak_record = Dashboard.where(user_id: session[:user_id], game_id: -1, streak_record: true).first
+      @dashboard_details["streak"] = streak_record&.streak_count || 0
+    end
+
+    def populate_last_played_on
+      last_played_record = Dashboard.where(user_id: session[:user_id]).where.not(game_id: -1).order(played_on: :desc).first
+      @dashboard_details["last_played_on"] = time_ago_in_words(last_played_record&.played_on)
+    end
+    
+    def populate_total_games_played
+      @dashboard_details["total_games_played"] = Dashboard.where(user_id: session[:user_id]).where.not(game_id: -1).count
+    end
+    
+    def populate_game_stats
       games = Game.where.not(id: -1)
       games.each do |game|
-        last_played_record = Dashboard.where(user_id: user_id, game_id: game.id).order(played_on: :desc).first
+        last_played_record = Dashboard.where(user_id: session[:user_id], game_id: game.id).order(played_on: :desc).first
         @dashboard_details[game.id] = {
           "name" => game.name,
           "last_played_on" => time_ago_in_words(last_played_record&.played_on),
-          "score" => Dashboard.where(user_id: user_id, game_id: game.id).sum(:score)
+          "score" => Dashboard.where(user_id: session[:user_id], game_id: game.id).sum(:score)
          }
       end
     end
